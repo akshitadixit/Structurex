aee6f6b04316f2f7866ab640478f9d5d01f5c21a
Aayush Jain <imjainaayush@gmail.com>
2021-02-02 14:37:38 +0530

    Prettier code

--- src/search/js/binarySearch.js
+++ src/search/js/binarySearch.js
@@ -1,36 +1,33 @@
 function binarysearch() {
     var l = 0;
-    var r = ARR.length-1;
+    var r = ARR.length - 1;
     var inputval = document.getElementById("texty").value;
 
-    while(l <= r) {
+    while (l <= r) {
         change(divs[l], ARR[l], "yellow");
         change(divs[r], ARR[r], "yellow");
-        
-        var mid = Math.floor((l+r)/2);
+
+        var mid = Math.floor((l + r) / 2);
         change(divs[mid], ARR[mid], "red");
-        // console.log(l, mid, r);
-        
-        if(ARR[mid] == inputval) {
+
+        if (ARR[mid] == inputval) {
             change(divs[l], ARR[l], "#40E0D0");
             change(divs[r], ARR[r], "#40E0D0");
             change(divs[mid], ARR[mid], "green");
             found(true, mid, inputval);
             return;
-        }    
-        else if(ARR[mid]<inputval) {
+        } else if (ARR[mid] < inputval) {
             change(divs[l], ARR[l], "#40E0D0");
             change(divs[r], ARR[r], "#40E0D0");
-            l = mid+1;
+            l = mid + 1;
+        } else {
+            change(divs[l], ARR[l], "#40E0D0");
+            change(divs[r], ARR[r], "#40E0D0");
+            r = mid - 1;
         }
-        else {
-            change(divs[l], ARR[l], "#40E0D0");
-            change(divs[r], ARR[r], "#40E0D0");
-            r = mid-1;
-        }   
-        change(divs[mid], ARR[mid], "#40E0D0");             
+        change(divs[mid], ARR[mid], "#40E0D0");
     }
 
     found(false, 0, inputval);
-    return ;
+    return;
 }
