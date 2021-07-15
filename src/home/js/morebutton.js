7654e8e1ec72209c832d9c7b253014e3870d0ecb
Sandhya Agarwal <sandhyaagarwal03180@gmail.com>
2021-07-15 17:08:16 +0530

    Resolved the conflicts

---
+++ src/home/js/morebutton.js
@@ -0,0 +1,11 @@
+
+function show_hide() {  
+
+	//used to toggle the dropdown list under more button
+   var click = document.getElementById("list-items");  
+   if(click.style.display ==="none") {  
+      click.style.display ="block";  
+   } else {  
+      click.style.display ="none";  
+   }   
+}  
